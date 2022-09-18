1524835176d9f83c0ac45c7f9f019708b7e7bbda
Marius van Remundt <marius_v_r@hotmail.com>
2022-09-18 21:34:50 +0200

    v0.2

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,9 @@ import "./ExpenseItem.css";
 function ExpenseItem() {
   return (
     <div>
-      <h1>This is an expense item!</h1>
+      <h1>March 2022 </h1>
+      <h2>XRD NFT 💥 </h2>
+      <h3>This is an expense item with a long description </h3>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import "./NavBar.css";
 
 function NavBar() {
   return (
-    <div class="navbar">
+    <div className="navbar">
       <p>Hello1</p>
       <p>Hello2</p>
       <p>Hello3</p>
